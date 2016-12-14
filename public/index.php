756cb28fce1727acfcaff10ddb661f046c43821f
Andrew Breksa <abreksa4@gmail.com>
2016-12-14 12:32:45 -0800

    update index.php with examples for new features

--- public/index.php
+++ public/index.php
@@ -1,7 +1,18 @@
 <?php
 require_once(__DIR__ . '/../vendor/autoload.php');
+
 //Create the App
 $app = \Unicorn\App\Application::getInstance();
+$app->bootstrap();
+
+//add something to "render" the output
+$app->getEventEmitter()->addListener(Unicorn\App\Application::EVENT_RENDER, function (\League\Event\Event $event, \Unicorn\App\Application $application) {
+	$application->getResponse()->getBody()->rewind();
+	$data = $application->getResponse()->getBody()->getContents();
+	$newBody = new \Zend\Diactoros\Stream(fopen('php://temp', 'r+'));
+	$newBody->write(json_encode(["data" => $data, 'data_from_getData()' => (array_key_exists('data1', $application->getData()) ? $application->getData()['data1'] : '')]));
+	$application->setResponse($application->getResponse()->withBody($newBody));
+});
 
 //add our GET:/ route
 $app->getContainer()->get('routeCollection')->map('GET', '/', function (\Psr\Http\Message\ServerRequestInterface $request, \Psr\Http\Message\ResponseInterface $response) {
@@ -15,9 +26,10 @@ $app->getEventEmitter()->addListener(\Unicorn\App\Application::EVENT_ROUTE_EXCEP
 	 * @var $response \Psr\Http\Message\ResponseInterface
 	 */
 	$response = $app->getContainer()->get('response');
+	$response = $response->withStatus(404);
 	$response->getBody()->write('404 - ' . $exception->getMessage());
-	$response = $response->withStatus(404, 'Not Found');
-	return $response;
+	$app->data['data1'] = 'this is a string';
+	$app->setResponse($response);
 });
 
 //register another route from an array (could be read from configuration, no?)
@@ -31,5 +43,6 @@ $app->bootstrapRoutes([
 		},
 	],
 ]);
+
 //run the App!
 $app->run();
